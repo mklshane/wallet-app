806ec9828e655cdb8506f5e6da788152be24dc4e
Mikaela Shane Estrellanes <mikaela_shane_estrellanes@dlsl.edu.ph>
2025-09-25 00:02:35 +0800

    >>

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,15 +1,14 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import { Link } from "expo-router";
+import { Image } from "expo-image";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+    <View 
     >
-      <Text>SKSKSKSK</Text>
+      
     </View>
   );
 }
+Text;
+
